4b23e513318a2a0fd176e436e664f7897109eba5
DEM Ahmed <demahmed02@gmail.com>
2026-04-12 18:37:36 +0200

    feat: setup routing system

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { Route, Routes } from "react-router"
+
 function App() {
 
   return (
-    <>
-      <div className="flex h-screen items-center justify-center">
-        <h1 className="text-4xl font-bold text-gray-800">Hello, World!</h1>
-      </div>
-    </>
+    <Routes>
+      <Route index element={<div>Home</div>} />
+      <Route path="auth" element={<div>Auth</div>} />
+      <Route path="dashboard" element={<div>Dashboard</div>} />
+      <Route path="projects" element={<div>Projects</div>} />
+      <Route path="builder" element={<div>Command Builder</div>} />
+      <Route path="templates" element={<div>Templates</div>} />
+      <Route path="documentation" element={<div>Documentation</div>} />
+      <Route path="settings" element={<div>Settings</div>} />
+      <Route path="support" element={<div>Support</div>} />
+    </Routes>
   )
 }
 
